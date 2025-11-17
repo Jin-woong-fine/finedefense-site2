a3f495db5666bfe4ebf3b661ba2de0e3888f593b
jinwoong <jwpark@finedefense.co.kr>
2025-11-18 08:08:18 +0900

    제품소개 업로드방식 동적 업데이트중

--- server/app.js
+++ server/app.js
@@ -1,4 +1,21 @@
 import express from "express";
+const app = express();
+
+// 🔥 uploads static 공개
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
+
+
+
+
+
+
+
+
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
