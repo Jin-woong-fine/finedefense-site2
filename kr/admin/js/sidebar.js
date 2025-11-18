9ba68b4fbb1ddc8912bb7f6981280379bb3644c2
jin-woong-fine <jwpark@finedefense.co.kr>
2025-11-18 18:24:10 +0900

    관리자 페이지 수정중

--- kr/admin/js/sidebar.js
+++ kr/admin/js/sidebar.js
@@ -4,17 +4,20 @@ function loadSidebar(activeMenu) {
   const container = document.getElementById("sidebar");
   if (!container) return;
 
-  // 관리자 사이드바 HTML을 직접 넣는다 (fetch, 인코딩 그런 거 전부 X)
+  // CSS에 맞춘 구조
   container.innerHTML = `
     <div class="sidebar">
-      <div class="sidebar-title">관리자 메뉴</div>
+
+      <div class="sidebar-logo">관리자 메뉴</div>
+
       <div class="sidebar-menu">
-        <a href="./dashboard.html" data-menu="dashboard">대시보드</a>
-        <a href="./products.html" data-menu="products">제품 관리</a>
-        <a href="./inquiry_list.html" data-menu="inquiry">1:1 문의</a>
-        <a href="./newsroom_list.html" data-menu="news">뉴스룸 관리</a>
-        <a href="./files.html" data-menu="files">자료실 관리</a>
+        <a href="./dashboard.html" class="menu-item" data-menu="dashboard">대시보드</a>
+        <a href="./products.html" class="menu-item" data-menu="products">제품 관리</a>
+        <a href="./inquiry_list.html" class="menu-item" data-menu="inquiry">1:1 문의</a>
+        <a href="./newsroom_list.html" class="menu-item" data-menu="news">뉴스룸 관리</a>
+        <a href="./files.html" class="menu-item" data-menu="files">자료실 관리</a>
       </div>
+
     </div>
   `;
 
@@ -27,5 +30,4 @@ function loadSidebar(activeMenu) {
   }
 }
 
-// 전역으로 노출 (type="module" 안 쓰고 그냥 <script>에서 쓰기 위함)
 window.loadSidebar = loadSidebar;
