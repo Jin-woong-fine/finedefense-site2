1c0e229fc1903b7a555f2108775f204411e4b21d
jinwoong <jwpark@finedefense.co.kr>
2025-11-18 14:20:04 +0900

    제품소개 업로드방식 동적 업데이트중

--- server/app.js
+++ server/app.js
@@ -25,8 +25,9 @@ const __dirname = path.dirname(__filename);
 // ------------------------------------------------------
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: true, limit: "20mb" }));
+
 
 // ------------------------------------------------------
 // 🔥 업로드 이미지 정적 제공
@@ -70,3 +71,8 @@ const PORT = 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
+
+
+
+
+
