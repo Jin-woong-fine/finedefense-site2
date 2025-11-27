75b30c8a0b2b917aa521c4678388effc24e8c7d4
jinwoong <jwpark@finedefense.co.kr>
2025-11-27 13:38:49 +0900

    문의 페이지 작성중

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,14 @@
+// server/db.js
 import mysql from "mysql2/promise";
-import dotenv from "dotenv";
-dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: "localhost",
+  user: "finedefense_user",
+  password: "fine!202310",
+  database: "finedefense",
   waitForConnections: true,
   connectionLimit: 10,
+  queueLimit: 0
 });
 
 export default pool;
