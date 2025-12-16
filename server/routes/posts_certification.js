fd659dcadeff39629ba617835d0fe938db8b33d8
jinwoong <jwpark@finedefense.co.kr>
2025-12-16 15:26:22 +0900

    관리자 인증 페이지 리스트에서 순번 수정

--- server/routes/posts_certification.js
+++ server/routes/posts_certification.js
@@ -5,7 +5,7 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
-import authMiddleware from "../middleware/authMiddleware.js";
+import authMiddleware from "../middleware/auth.js";
 
 
 const router = express.Router();
