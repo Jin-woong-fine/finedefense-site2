cc6e3a3d2d004b3fb5bb14922b45cca456438408
jinwoong <jwpark@finedefense.co.kr>
2025-12-19 08:45:44 +0900

    뉴스 리스트 삭제는 슈퍼애드민만 보이게

--- server/routes/posts_notice.js
+++ server/routes/posts_notice.js
@@ -5,7 +5,8 @@ import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
 import db from "../config/db.js";
-router.delete("/delete/:id", verifyToken, canDelete, async (req, res) => {
+import { verifyToken, canDelete } from "../middleware/auth.js";
+
 
 const router = express.Router();
 
@@ -169,7 +170,11 @@ router.put("/update/:id", verifyToken, uploadNotice.array("files", 10), async (r
 // ============================================================
 // 📌 공지 삭제
 // ============================================================
-router.delete("/delete/:id", verifyToken, canDelete, async (req, res) => {
+router.delete(
+  "/delete/:id",
+  verifyToken,
+  canDelete,
+  async (req, res) => {
   try {
     const id = Number(req.params.id);
 
